792a1c9f153d8c030b7398a043f3b0314b0a944c
noavish <noavish1@gmail.com>
2018-01-01 23:23:29 +0200

    search by title, display first 10 books

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
-var fetch = function (isbnNumber) {
+var fetch = function (bookName) {
     $.ajax({
         method: "GET",
-        url: `https://www.googleapis.com/books/v1/volumes?q=isbn:${isbnNumber}`,
+        url: `https://www.googleapis.com/books/v1/volumes?q=title:${bookName}`,
         success: function (data) {
+            $('.book').empty();
             console.log(data);
             createBook(data);
         },
@@ -13,17 +14,19 @@ var fetch = function (isbnNumber) {
 };
 
 $('.search-book').click(function () {
-    var isbnNumber = $('input[id="isbn"]').val();
-    fetch(isbnNumber);
+    var bookName = $('input[id="isbn"]').val();
+    fetch(bookName);
 });
 
 function createBook(data) {
-    var title = data.items[0].volumeInfo.title;
-    var description = data.items[0].volumeInfo.description;
-    var authors = data.items[0].volumeInfo.authors;
-    var imageLinks = data.items[0].volumeInfo.imageLinks.thumbnail;
-    var bookObject = { title: title, description: description, authors: authors, imageLinks: imageLinks };
-    appendBook(bookObject);
+    for (var i=0; i<10; i++) {
+        var title = data.items[i].volumeInfo.title;
+        var description = data.items[i].volumeInfo.description;
+        var authors = data.items[i].volumeInfo.authors;
+        var imageLinks = data.items[i].volumeInfo.imageLinks.thumbnail;
+        var bookObject = { title: title, description: description, authors: authors, imageLinks: imageLinks };
+        appendBook(bookObject);
+    }
 }
 
 function appendBook (bookObject) {
