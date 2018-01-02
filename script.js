16c754a278bf251a6b6dfdb79d195e5671955360
noavish <noavish1@gmail.com>
2018-01-02 02:06:28 +0200

    search by different fields + loading gif

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
-var fetch = function (bookName) {
+var fetch = function (searchKey, searchTerm) {
     $.ajax({
         method: "GET",
-        url: `https://www.googleapis.com/books/v1/volumes?q=title:${bookName}`,
+        url: `https://www.googleapis.com/books/v1/volumes?q=${searchKey}:${searchTerm}`,
         success: function (data) {
-            $('.book').empty();
             console.log(data);
             createBook(data);
         },
@@ -13,17 +12,16 @@ var fetch = function (bookName) {
     });
 };
 
-$('.search-book').click(function () {
-    var bookName = $('input[id="isbn"]').val();
-    fetch(bookName);
-});
-
 function createBook(data) {
-    for (var i=0; i<10; i++) {
+    $('.book').empty();
+    for (var i=0; i<10 && i<data.items.length; i++) {
         var title = data.items[i].volumeInfo.title;
         var description = data.items[i].volumeInfo.description;
         var authors = data.items[i].volumeInfo.authors;
-        var imageLinks = data.items[i].volumeInfo.imageLinks.thumbnail;
+        var imageLinks = '';
+        if ('imageLinks' in data.items[i].volumeInfo) {
+            imageLinks = data.items[i].volumeInfo.imageLinks.thumbnail;
+        }
         var bookObject = { title: title, description: description, authors: authors, imageLinks: imageLinks };
         appendBook(bookObject);
     }
@@ -35,5 +33,26 @@ function appendBook (bookObject) {
     var newHTML = bookObjectTemplate(bookObject);
     $('.book').append(newHTML);
 }
+
+var searchKey = '';
+
+$( ".form-control" ).focus(function() {
+    searchKey = this.id; 
+    $(`input[id=${searchKey}]`).css("background-color", "#fff");
+    $(`input[id!=${searchKey}]`).css("background-color", "#ccc"); 
+});
+
+$('.search-book').click(function () {
+    var searchTerm = $(`input[id=${searchKey}]`).val();
+    fetch(searchKey, searchTerm);
+    $('.book').empty();
+    $('.book').append('<img src="loading.gif">');
+});
+
+$('.book').on('click', '.book-title', function () {
+    var specificBook = $(this).parents('.spesific-book');
+    $('.book').empty();
+    $('.book').append(specificBook);    
+});
 
 
